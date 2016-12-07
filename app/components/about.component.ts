631c696c70af99af52bce42e4c4bf48540a92ee6
Marco Berardini <mberardini@evolution-man.com>
2016-12-06 23:16:53 -0800

    [client] Added About page content and minor css changes.

--- app/components/about.component.ts
+++ app/components/about.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core'
     <header>
         <h1>About</h1>
         <h5>Developer: Marco Berardini</h5>
-        <a href="https://github/mb0606">View my profile on Github</a>
+        <a href="https://github/mb0606" target="_blank">View my profile on Github</a>  | 
+        <a href="https://github.com/mb0606/musicFind" target="_blank">View Project Repo on Github</a>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci, assumenda corporis culpa cumque esse, ex expedita harum incidunt maxime mollitia nulla numquam quaerat quibusdam rerum, similique tempora tenetur veritatis.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quidem reiciendis sint. Beatae blanditiis cumque doloribus enim excepturi facilis id itaque nemo quis repudiandae sit, tempore! Aperiam repellendus reprehenderit repudiandae.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi culpa dolore eos explicabo impedit magni omnis quos repellendus sunt voluptate. Ad excepturi praesentium suscipit. Assumenda esse necessitatibus neque rerum. Architecto!</p>
